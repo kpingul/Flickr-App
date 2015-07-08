21471c4d2bb79f4d09dbe5f0cba0dcc932ec1e16
kpingul <kirckpingul@gmail.com>
2015-07-08 16:12:04 -0700

    minified all CSS dependencies and stylesheets

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp 		= require('gulp'),
 	concat 		= require('gulp-concat'),
 	rename 		= require('gulp-rename'),
-	uglify 		= require('gulp-uglify');
+	uglify 		= require('gulp-uglify'),
+    minifyCss 	= require('gulp-minify-css');
 
 
 /* Define Tasks*/
@@ -39,9 +40,47 @@ gulp.task('depsDist', function(){
 
 gulp.task('scriptJs', function(){
 	return gulp.src(['src/js/flickr.js'])
+
 			.pipe(concat('flickr.min.js'))
 			.pipe(uglify())
 			.pipe(gulp.dest('bin'));
 
+});
+
+
+//Minify CSS files
+
+
+//minify css dependencies
+
+gulp.task('depsCSS', function(){
+
+	return gulp.src([
+			'bower_components/bootstrap/dist/css/bootstrap.min.css',
+			'bower_components/blueimp-gallery/css/blueimp-gallery.min.css'
+		])
+		.pipe(concat('deps.css'))
+		.pipe(gulp.dest('src'));
+
 })
 
+gulp.task('depsDistCss', function(){
+	return gulp.src(['src/deps.css'])
+
+			.pipe(rename('deps.min.css'))
+			.pipe(minifyCss())
+			.pipe(gulp.dest('bin'));
+})
+
+
+
+
+//minify site stylsheets
+gulp.task('linkCSS', function(){
+	return gulp.src(['src/css/stylesheet.css'])
+
+			.pipe(rename('stylesheet.min.css'))
+			.pipe(minifyCss())
+			.pipe(gulp.dest('bin'));
+
+});
